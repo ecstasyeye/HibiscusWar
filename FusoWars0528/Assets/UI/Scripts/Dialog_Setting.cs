37e38fee823140f768f824161a5e4c8e67bb0d72
a1856707 <a1856707@hotmail.com>
2017-07-02 21:50:22 +0800

    新增主選單

--- FusoWars0528/Assets/UI/Scripts/Dialog_Setting.cs
+++ FusoWars0528/Assets/UI/Scripts/Dialog_Setting.cs
@@ -8,23 +8,27 @@ public class Dialog_Setting : MonoBehaviour
     Toggle Toggle_Window;
     Slider Slider_Music;
     Slider Slider_Sound;
-    Button Button_OK;
+    Button Button_Back;
+    Button Button_BackMenu;
+    GameObject Panel;
     bool IsFullScreen = true;
 
     // Use this for initialization
     void Start ()
     {
+        Panel = transform.Find("Panel").gameObject;
         Toggle_FullScreen = transform.Find("Panel/Toggle_FullScreen").GetComponent<Toggle>();
         Toggle_Window = transform.Find("Panel/Toggle_Window").GetComponent<Toggle>();
         Slider_Music = transform.Find("Panel/Slider_Music").GetComponent<Slider>();
         Slider_Sound = transform.Find("Panel/Slider_Sound").GetComponent<Slider>();
-        Button_OK = transform.Find("Panel/Button_Back").GetComponent<Button>();
-        Button_OK = transform.Find("Panel/Button_BackMenu").GetComponent<Button>();
+        Button_Back = transform.Find("Panel/Button_Back").GetComponent<Button>();
+        Button_BackMenu = transform.Find("Panel/Button_BackMenu").GetComponent<Button>();
         Toggle_FullScreen.onValueChanged.AddListener(Toggle_FullScreen_OnValueChanged);
         Toggle_Window.onValueChanged.AddListener(Toggle_Window_OnValueChanged);
         Slider_Music.onValueChanged.AddListener(Slider_Music_OnValueChanged);
         Slider_Sound.onValueChanged.AddListener(Slider_Sound_OnValueChanged);
-        Button_OK.onClick.AddListener(delegate (){this.OnClick(Button_OK.gameObject);});
+        Button_Back.onClick.AddListener(delegate () { this.OnClick(Button_Back.gameObject); });
+        Button_BackMenu.onClick.AddListener(delegate (){this.OnClick(Button_BackMenu.gameObject);});
         Screen.fullScreen = IsFullScreen;
     }
 
@@ -62,8 +66,10 @@ public class Dialog_Setting : MonoBehaviour
         switch (sender.name)
         {
             case "Button_Back":
+                Panel.SetActive(false);
                 break;
             case "Button_BackMenu":
+                Panel.SetActive(false);
                 break;
         }
     }
---
+++ FusoWars0528/Assets/UI/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject dialog_Setting;
+
+    // Use this for initialization
+    void Start()
+    {
+        // 绑定buttons event  
+        Button[] buttons = GetComponentsInChildren<Button>(true);
+        foreach (var item in buttons)
+        {
+            Button btn = item;
+            btn.onClick.AddListener(delegate ()
+            {
+                this.OnClick(btn.gameObject);
+            });
+        }
+    }
+
+    public void OnClick(GameObject sender)
+    {
+        switch (sender.name)
+        {
+            case "Button_Start":
+                break;
+            case "Button_Load":
+                break;
+            case "Button_Setting":
+                dialog_Setting.SetActive(true);
+                break;
+            case "Button_End":
+                Application.Quit();
+                break;
+        }
+    }
+}
